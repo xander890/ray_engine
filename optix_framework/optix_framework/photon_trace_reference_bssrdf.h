5b7b007f43a70dba09f060f649553230ee6b28af
Alessandro Dal Corso <alcor@dtu.dk>
2017-10-05 20:39:12 +0900

    bug that was infinite looping on total internal refraction.

--- optix_framework/optix_framework/photon_trace_reference_bssrdf.h
+++ optix_framework/optix_framework/photon_trace_reference_bssrdf.h
@@ -59,7 +59,7 @@ __forceinline__ __device__ bool scatter_photon(optix::float3& xp, optix::float3&
 
 			if (sin_theta_o_sqr >= 1.0f) // Total internal reflection, no accumulation
 			{
-				continue;
+				optix_print("(%d) Internal refr.\n", i);
 			}
 			else
 			{
