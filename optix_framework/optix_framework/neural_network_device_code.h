872ec1a43e8df9f3354bb0d22b9e11679d274089
a21572 <chakravarty.alla@gmail.com>
2018-01-05 23:06:08 +0900

    Added support for GPU inference

--- optix_framework/optix_framework/neural_network_device_code.h
+++ optix_framework/optix_framework/neural_network_device_code.h
@@ -6,6 +6,318 @@
 #include <scattering_properties.h>
 #include <sampling_helpers.h>
 
+// HyperNetwork parameters
+rtDeclareVariable(int, hypernetwork_num_layers, , );
+
+rtBuffer<float> hypernetwork_layer1_weights;
+rtDeclareVariable(int, hypernetwork_layer1_in_size, ,);
+rtDeclareVariable(int, hypernetwork_layer1_out_size, ,);
+rtBuffer<float> hypernetwork_layer1_biases;
+
+rtBuffer<float> hypernetwork_layer2_weights;
+rtDeclareVariable(int, hypernetwork_layer2_in_size, ,);
+rtDeclareVariable(int, hypernetwork_layer2_out_size, ,);
+rtBuffer<float> hypernetwork_layer2_biases;
+
+rtBuffer<float> hypernetwork_layer3_weights;
+rtDeclareVariable(int, hypernetwork_layer3_in_size, ,);
+rtDeclareVariable(int, hypernetwork_layer3_out_size, ,);
+rtBuffer<float> hypernetwork_layer3_biases;
+
+__device__ __forceinline__ void mat_mul(int rows, int cols, int inner, float *a, float *b, float *c)
+{
+    for (int i = 0; i < rows; ++i)
+    {
+        for (int j = 0; j < cols; ++j)
+        {
+            c[i * cols + j] = 0.0;
+            for (int k = 0; k < inner; ++k)
+            {   
+                c[i * cols + j] += a[i * inner + k] * b[k * cols + j];
+            }
+        }
+    }
+}
+
+__device__ __forceinline__ void set_cdfnetwork_parameters(
+        int layer_index,     // CDF network layer index
+        int num_layers,      // CDF network number of layers
+        float *params,       // Hypernetwork output parameters 
+        int in_size,         // CDF network layer dimensions
+        int out_size,
+        optix::Matrix4x4 &W, // CDF network layer weights
+        float *b,            // CDF network layer biases
+        float *c)            // CDF network layer additional learnable weights
+{
+    // Stride begins from the front
+    int initial_stride = hypernetwork_layer3_out_size / num_layers * (layer_index);
+
+    // Weights 
+    for (int i = 0; i < in_size; ++i)
+    {
+        for (int j = 0; j < out_size; ++j)
+        {
+            int index = i * out_size + j;
+            W[index] = params[initial_stride + index];
+        }
+    }
+
+    // Biases and additional learnable weights
+    int len_W = in_size * out_size;
+    for (int i = 0; i < out_size; ++i)
+    {
+        b[i] = params[initial_stride + len_W + i];
+        c[i] = params[initial_stride + len_W + out_size + i];
+    }
+}
+
+__device__ __forceinline__ void get_cdfnetwork_output(float *input, float *params, float *output)
+{
+    // CDF network dimensions
+    int D_in = 4;
+    int H = 4;
+    int D_out = 4;
+    int num_layers = 3;
+
+    // Set CDF network parameters
+    optix::Matrix4x4 cdfnetwork_W[3];
+    float cdfnetwork_b[3][4];
+    float cdfnetwork_c[3][4];
+
+    set_cdfnetwork_parameters(0, num_layers, params, D_in, H, cdfnetwork_W[0], &cdfnetwork_b[0][0], &cdfnetwork_c[0][0]);
+    set_cdfnetwork_parameters(1, num_layers, params, H, H, cdfnetwork_W[1], &cdfnetwork_b[1][0], &cdfnetwork_c[1][0]);
+    set_cdfnetwork_parameters(2, num_layers, params, H, D_out, cdfnetwork_W[2], &cdfnetwork_b[2][0], &cdfnetwork_c[2][0]);
+
+    // Layer 1: Expression Layer
+    //     input: 1 x D_in (dimensions)
+    //     output: 1 x D_in
+
+    //     Map from (0, 1) to (-inf, inf)
+    //     s2 = atanh(2 * input - 1)
+    float s2[4];
+    for (int i = 0; i < D_in; ++i)
+    {
+        s2[i] = atanh(2.0 * input[i] - 1.0);
+    }
+
+    // Layer 2: Linear Layer
+    //     input: 1 x D_in (dimensions)
+    //     weights: D_in x H
+    //     biases: 1 x H
+    //     c: 1 x H
+    //     output: 1 x H
+
+    //     Hyperbolic tangent nonlinear activation
+    //     s3 = asinh(c1 + sinh(b1 + s2.dot(W1)))
+    float s3[4];
+    mat_mul(1, H, D_in, &s2[0], &cdfnetwork_W[0][0], &s3[0]);
+
+    for (int i = 0; i < H; ++i)
+    {
+        s3[i] = asinh(cdfnetwork_c[0][i] + sinh(cdfnetwork_b[0][i] + s3[i]));
+    }
+
+    // Layer 3: Linear Layer
+    //     input: 1 x H (dimensions)
+    //     weights: H x H
+    //     biases: 1 x H
+    //     c: 1 x H
+    //     output: 1 x H
+
+    //     Hyperbolic tangent nonlinear activation
+    //     s4 = asinh(c2 + sinh(b2 + s3.dot(W2)))
+    float s4[4];
+    mat_mul(1, H, H, &s3[0], &cdfnetwork_W[1][0], &s4[0]);
+
+    for (int i = 0; i < H; ++i)
+    {
+        s4[i] = asinh(cdfnetwork_c[1][i] + sinh(cdfnetwork_b[1][i] + s4[i]));
+    }
+
+    // Layer 4: Linear Layer
+    //     input: 1 x H (dimensions)
+    //     weights: H x D_out
+    //     biases: 1 x D_out
+    //     c: 1 x D_out
+    //     output: 1 x D_out
+
+    //     Hyperbolic tangent nonlinear activation
+    //     s5 = asinh(c3 + sinh(b3 + s4.dot(W3)))
+    float s5[4];
+    mat_mul(1, D_out, H, &s4[0], &cdfnetwork_W[2][0], &s5[0]);
+
+    for (int i = 0; i < D_out; ++i)
+    {
+        s5[i] = asinh(cdfnetwork_c[2][i] + sinh(cdfnetwork_b[2][i] + s5[i]));
+    }
+
+    // Layer 5: Expression Layer
+    //     input: 1 x D_out (dimensions)
+    //     output: 1 x D_out
+
+    //     Map from (-inf, inf) to (0, 1)
+    //     output = tanh(s5) * 0.5 + 0.5
+    for (int i = 0; i < D_out; ++i)
+    {
+        output[i] = tanh(s5[i]) * 0.5 + 0.5;
+    }
+}
+
+__device__ __forceinline__ void get_icdfnetwork_output(float *input, float *params, float *output)
+{
+    // CDF network dimensions
+    int D_in = 4;
+    int H = 4;
+    int D_out = 4;
+    int num_layers = 3;
+
+    // Set CDF network parameters
+    optix::Matrix4x4 cdfnetwork_W[3];
+    float cdfnetwork_b[3][4];
+    float cdfnetwork_c[3][4];
+
+    set_cdfnetwork_parameters(0, num_layers, params, D_in, H, cdfnetwork_W[2], &cdfnetwork_b[2][0], &cdfnetwork_c[2][0]);
+    set_cdfnetwork_parameters(1, num_layers, params, H, H, cdfnetwork_W[1], &cdfnetwork_b[1][0], &cdfnetwork_c[1][0]);
+    set_cdfnetwork_parameters(2, num_layers, params, H, D_out, cdfnetwork_W[0], &cdfnetwork_b[0][0], &cdfnetwork_c[0][0]);
+
+    // Layer 1: Expression Layer
+    //     input: 1 x D_in (dimensions)
+    //     output: 1 x D_in
+
+    //     Map from (0, 1) to (-inf, inf)
+    //     s2 = atanh(2 * input - 1)
+    float s2[4];
+    for (int i = 0; i < D_in; ++i)
+    {
+        s2[i] = atanh(2.0 * input[i] - 1.0);
+    }
+
+    // Layer 2: Linear Layer
+    //     input: 1 x D_in (dimensions)
+    //     weights: D_in x H
+    //     biases: 1 x H
+    //     c: 1 x H
+    //     output: 1 x H
+
+    //     Hyperbolic tangent nonlinear activation
+    //     s3 = asinh(-c3 + sinh(s2))
+    //     s3 = s3 - b3
+    //     s3 = s3.dot(inv(W3))
+    float s3[4];
+    for (int i = 0; i < D_in; ++i)
+    {
+        output[i] = asinh(-cdfnetwork_c[0][i] + sinh(s2[i]));
+        output[i] -= cdfnetwork_b[0][i];
+    }
+
+    optix::Matrix4x4 temp = cdfnetwork_W[0].inverse();
+    mat_mul(1, H, D_in, &output[0], &temp[0], &s3[0]);
+
+    // Layer 3: Linear Layer
+    //     input: 1 x H (dimensions)
+    //     weights: H x H
+    //     biases: 1 x H
+    //     c: 1 x H
+    //     output: 1 x H
+
+    //     Hyperbolic tangent nonlinear activation
+    //     s4 = asinh(-c2 + sinh(s3))
+    //     s4 = s4 - b2
+    //     s4 = s4.dot(inv(W2))
+    float s4[4];
+    for (int i = 0; i < H; ++i)
+    {
+        output[i] = asinh(-cdfnetwork_c[1][i] + sinh(s3[i]));
+        output[i] -= cdfnetwork_b[1][i];
+    }
+
+    temp = cdfnetwork_W[1].inverse();
+    mat_mul(1, H, H, &output[0], &temp[0], &s4[0]);
+
+    // Layer 3: Linear Layer
+    //     input: 1 x H (dimensions)
+    //     weights: H x D_out
+    //     biases: 1 x D_out 
+    //     c: 1 x D_out
+    //     output: 1 x D_out
+
+    //     Hyperbolic tangent nonlinear activation
+    //     s5 = asinh(-c1 + sinh(s4))
+    //     s5 = s5 - b1
+    //     s5 = s5.dot(inv(W1))
+    float s5[4];
+    for (int i = 0; i < H; ++i)
+    {
+        output[i] = asinh(-cdfnetwork_c[2][i] + sinh(s4[i]));
+        output[i] -= cdfnetwork_b[2][i];
+    }
+
+    temp = cdfnetwork_W[2].inverse();
+    mat_mul(1, D_out, H, &output[0], &temp[0], &s5[0]);
+
+    // Layer 5: Expression Layer
+    //     input: 1 x D_out (dimensions)
+    //     output: 1 x D_out
+
+    //     Map from (-inf, inf) to (0, 1)
+    //     output = tanh(s5) * 0.5 + 0.5
+    for (int i = 0; i < D_out; ++i)
+    {
+        output[i] = tanh(s5[i]) * 0.5 + 0.5;
+    }
+}
+
+__device__ __forceinline__ void get_hypernetwork_output(float *input, float *output)
+{
+    // Layer 1: DenseLayer
+    //    input: 1 x 4 (dimensions)
+    //    weights: hypernetwork_layer1_in_size x hypernetwork_layer1_out_size
+    //    biases: 1 x hypernetwork_layer1_out_size
+    //    output: 1 x hypernetwork_layer1_out_size
+
+    //    s2 = input.dot(weights) + biases
+    //    s2 = sigmoid(s2)
+    float s2[6];
+    mat_mul(1, hypernetwork_layer1_out_size, hypernetwork_layer1_in_size, input, &hypernetwork_layer1_weights[0], &s2[0]);
+
+    for (int i = 0; i < hypernetwork_layer1_out_size; ++i)
+    {   
+        s2[i] += hypernetwork_layer1_biases[i];
+        s2[i] = 1.0 / (1.0 + exp(-s2[i]));
+    }
+
+    // Layer 2: DenseLayer
+    //    input: 1 x 6 (dimensions)
+    //    weights: hypernetwork_layer2_in_size x hypernetwork_layer2_out_size
+    //    biases: 1 x hypernetwork_layer2_out_size
+    //    output: 1 x hypernetwork_layer2_out_size
+
+    //    s3 = s2.dot(weights) + biases
+    //    s3 = sigmoid(s3)
+    float s3[6];
+    mat_mul(1, hypernetwork_layer2_out_size, hypernetwork_layer2_in_size, &s2[0], &hypernetwork_layer2_weights[0], &s3[0]);
+    
+    for (int i = 0; i < hypernetwork_layer2_out_size; ++i)
+    {   
+        s3[i] += hypernetwork_layer2_biases[i];
+        s3[i] = 1.0 / (1.0 + exp(-s3[i]));
+    }
+
+    // Layer 3: DenseLayer
+    //    input: 1 x 6 (dimensions)
+    //    weights: hypernetwork_layer3_in_size x hypernetwork_layer3_out_size
+    //    biases: 1 x hypernetwork_layer3_out_size
+    //    output: 1 x hypernetwork_layer3_out_size
+
+    //    output = s3.dot(weights) + biases
+    mat_mul(1, hypernetwork_layer3_out_size, hypernetwork_layer3_in_size, &s3[0], &hypernetwork_layer3_weights[0], output);
+    
+    for (int i = 0; i < hypernetwork_layer3_out_size; ++i)
+    {
+        output[i] += hypernetwork_layer3_biases[i];
+    }
+}
+
 __device__ __forceinline__ float map_interval(float point, optix::float2 interval_from, optix::float2 interval_to)
 {
     float map_01 = (point - interval_from.x)/(interval_from.y - interval_from.x);
@@ -38,20 +350,56 @@ __device__ __forceinline__ void sample_neural_network(
     float x3 = sampler->next1D();
     float x4 = sampler->next1D();
 
-    optix::float4 hypernetwork_parameters = make_float4(eta,g,albedo,theta_i);
-    optix::float4 cdfnetwork_parameters = make_float4(x1,x2,x3,x4);
+    // Get hypernetwork output for the specified optical parameters. A set of
+    // optical parameters are passed to the hypernetwork which will generate
+    // parameters for the iCDF network.
+    // The hypernetwork output dimensions should match 
+    // hypernetwork_layer3_out_size. Preallocated arrays are used here instead
+    // of dynamic memory allocation.
+    float hypernetwork_input[4] = {eta, g, albedo, theta_i};
+    float hypernetwork_output[72];
+    get_hypernetwork_output(&hypernetwork_input[0], &hypernetwork_output[0]);
 
-    // Sample neural network here!
-    float y1 = 0, y2 = 0, y3 = 0, y4 = 0; 
+    // Get iCDF network output. We pass a set of random numbers through the
+    // iCDF network to get the geometry parameters. We then use these geometry
+    // parameters to get an outgoing direction.
+    float icdfnetwork_input[4] = {x1, x2, x3, x4};
+    float icdfnetwork_output[4];
+    get_icdfnetwork_output(&icdfnetwork_input[0], &hypernetwork_output[0], &icdfnetwork_output[0]);
+
+    // Get CDF network output to verify iCDF network
+    float cdfnetwork_output[4];
+    get_cdfnetwork_output(&icdfnetwork_output[0], &hypernetwork_output[0], &cdfnetwork_output[0]);
+
+    #if 0
+    for (int i = 0; i < hypernetwork_layer3_out_size; ++i)
+    {
+        optix_print("%f ", hypernetwork_output[i]);
+    }
+    optix_print("\n");
+    #endif
+    optix_print("icdfnetwork_input: %f %f %f %f\n", x1, x2, x3, x4);
+    optix_print("icdfnetwork_output: ");
+    for (int i = 0; i < 4; ++i)
+    {
+        optix_print("%f ", icdfnetwork_output[i]);
+    }
+    optix_print("\n");
+    optix_print("cdfnetwork_output: ");
+    for (int i = 0; i < 4; ++i)
+    {
+        optix_print("%f ", cdfnetwork_output[i]);
+    }
+    optix_print("\n");
 
     // Also here sample normalization constant
     float bssrdf_integral = 0.0f;
     integration_factor *= bssrdf_integral;
 
-    float r = map_interval(y1, optix::make_float2(0,1), optix::make_float2(0.01f, 10.0f));           
-    float theta_s = map_interval(y2, optix::make_float2(0,1), optix::make_float2(0.0f, M_PIf));           
-    float theta_o = map_interval(y3, optix::make_float2(0,1), optix::make_float2(0.0f, M_PIf/2));           
-    float phi_o = map_interval(y4, optix::make_float2(0,1), optix::make_float2(0.0f, 2*M_PIf));           
+    float r = map_interval(icdfnetwork_output[0], optix::make_float2(0,1), optix::make_float2(0.01f, 10.0f));           
+    float theta_s = map_interval(icdfnetwork_output[1], optix::make_float2(0,1), optix::make_float2(0.0f, M_PIf));           
+    float theta_o = map_interval(icdfnetwork_output[2], optix::make_float2(0,1), optix::make_float2(0.0f, M_PIf/2));           
+    float phi_o = map_interval(icdfnetwork_output[3], optix::make_float2(0,1), optix::make_float2(0.0f, 2*M_PIf));           
 
     // The radius is expressed in mean free paths, so we renormalize it.
     r /= extinction;
--- optix_framework/optix_framework/neural_network_sampler.cpp
+++ optix_framework/optix_framework/neural_network_sampler.cpp
@@ -1,5 +1,113 @@
 #include "neural_network_sampler.h"
 #include "logger.h"
+#include <Eigen/Dense>
+#include <fstream>
+
+using Eigen::MatrixXf;
+
+bool exists(const char *fileName)
+{
+    // Check if a file exists
+    std::ifstream infile(fileName);
+    return infile.good();
+}
+
+NeuralNetworkSampler::NeuralNetworkSampler(optix::Context & ctx) : mContext(ctx)
+{
+    // Retrieve hypernetwork parameters
+    std::string filepath(Folders::data_folder);
+    std::string filename("parameters.bin");
+    std::string network_path = filepath + filename;
+
+    // Check if the trained network file exists or not
+    if (exists(network_path.c_str()))
+    {
+        // Neural Network trained file format (binary)
+        // Contents:
+        //     n (number of hypernetwork parameters)
+
+        // For each layer there will be corresponding weights and biases.
+        //     in_size out_size (weights' dimensions)
+        //     weights
+
+        //     in_size out_size (biases' dimensions)
+        //     biases
+        FILE *fp = fopen(network_path.c_str(), "rb");
+        int params_size;
+        int num_layers;
+
+        // Get the number of layers
+        fread(&params_size, sizeof(int), 1, fp);
+        num_layers = int(params_size / 2);
+
+        // Create place holders for hypernetwork parameters
+        MatrixXf *weights = new MatrixXf[num_layers];
+        MatrixXf *biases = new MatrixXf[num_layers];
+
+        // Allocate storage for OptiX buffers
+        mHyperNetworkWeights.resize(num_layers);
+        mHyperNetworkBiases.resize(num_layers);
+        mContext["hypernetwork_num_layers"]->setInt(num_layers);
+
+        // Read hypernetwork parameters
+        for (int i = 0, layer_index = 0; i < params_size; ++i)
+        {
+            // Retrieve parameters' dimensions
+            int in_size;
+            int out_size;
+            int total_size;
+            char name[1024];
+
+            fread(&in_size, sizeof(int), 1, fp);
+            fread(&out_size, sizeof(int), 1, fp);
+            total_size = in_size * out_size;
+
+            if (i % 2 == 0)
+            {
+                // Initialize weights
+                MatrixXf &weight = weights[layer_index];
+                weight = MatrixXf::Zero(out_size, in_size);
+
+                fread(weight.data(), sizeof(float), total_size, fp);
+
+                // Create OptiX buffers
+                mHyperNetworkWeights[layer_index] = mContext->createBuffer(RT_BUFFER_INPUT, RT_FORMAT_FLOAT, total_size);
+                memcpy(mHyperNetworkWeights[layer_index]->map(), weight.transpose().data(), sizeof(float) * total_size);
+                mHyperNetworkWeights[layer_index]->unmap();
+
+                // Send data to GPU
+                sprintf(name, "hypernetwork_layer%d_weights", layer_index + 1);
+                mContext[std::string(name)]->setBuffer(mHyperNetworkWeights[layer_index]);
+
+                sprintf(name, "hypernetwork_layer%d_in_size", layer_index + 1);
+                mContext[std::string(name)]->setInt(in_size);
+
+                sprintf(name, "hypernetwork_layer%d_out_size", layer_index + 1);
+                mContext[std::string(name)]->setInt(out_size);
+            }
+            else
+            {
+                // Assign biases
+                MatrixXf &bias = biases[layer_index];
+                bias = MatrixXf::Zero(in_size, out_size);
+
+                fread(bias.data(), sizeof(float), total_size, fp);
+
+                // Create OptiX buffers
+                mHyperNetworkBiases[layer_index] = mContext->createBuffer(RT_BUFFER_INPUT, RT_FORMAT_FLOAT, total_size);
+                memcpy(mHyperNetworkBiases[layer_index]->map(), bias.data(), sizeof(float) * total_size);
+                mHyperNetworkBiases[layer_index]->unmap();
+
+                // Send to GPU
+                sprintf(name, "hypernetwork_layer%d_biases", layer_index + 1);
+                mContext[std::string(name)]->setBuffer(mHyperNetworkBiases[layer_index]);
+
+                ++layer_index;
+            }
+        }
+        fclose(fp);
+    }
+}
 
 bool NeuralNetworkSampler::on_draw()
 {
